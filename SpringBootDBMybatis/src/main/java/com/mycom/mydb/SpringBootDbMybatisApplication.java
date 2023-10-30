2a5395dc34f8561e3cf1abd920389e634ff7a387
arimkim <kal72222222@gmail.com>
2023-10-30 17:22:29 +0900

    spring

---
+++ SpringBootDBMybatis/src/main/java/com/mycom/mydb/SpringBootDbMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.mydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDbMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDbMybatisApplication.class, args);
+	}
+
+}
---
+++ SpringBootDBMybatis/src/main/java/com/mycom/mydb/service/EmpService.java
@@ -0,0 +1,23 @@
+package com.mycom.mydb.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.mycom.mydb.dto.EmpDto;
+
+public interface EmpService {
+
+	EmpDto empDetail(int employeeId); // 사원 상세 정보
+
+	List<EmpDto> empList(); // 사원 리스트(목록)
+
+	int empInsert(EmpDto dto); // 사원 등록
+
+	int empUpdate(EmpDto dto); // 사원 수정
+
+	int empDelete(int employeeId); // 사원 삭제
+
+
+}
